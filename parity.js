6f9153858fb7323454acdad4b1665acbead8517b
bilaz666 <victor.me.marc@gmail.com>
2021-03-03 19:24:33 +0100

    commit

--- parity.js
+++ parity.js
@@ -1,23 +1,11 @@
-/*
 const isOdd = (nb) => {
-if (nb%2 == 0 ){
-return `false : le chiffre ${nb} est paire.`;
-}else{
-  return `true : le chiffre ${nb} est bien impaire.`;
-}
+  return (nb%2 == 0 ? false : true);
 }
 
-console.log(isOdd(5))
-*/
+console.log(isOdd(6))
 
-const isOdd = (nb) => {
-  return (nb%2 == 0 ? 'false' : 'true');
+
+const isEven = (nb) => {
+return (isOdd(nb) ? false : true)
 }
-
-//console.log(isOdd(5))
-
-const isEven = (nb1) => {
-  return (isOdd ? 'true' : 'false')
-}
-console.log(isEven(5))
-//non fini
+console.log(isEven(6))
